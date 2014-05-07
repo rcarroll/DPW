6b04c148ce7bb4d07d4f95595da1a7406e5e4e7d
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-05-06 21:42:14 -0400

    Stat Prompt's

--- Rodriguez_Julian_Madlib/main.py
+++ Rodriguez_Julian_Madlib/main.py
@@ -13,3 +13,8 @@ print "Ohh so you are a mighty, "+CLASS+", Very interesting..."
 HOME= raw_input("Where do you hail from?:")
 print "I see so you come from the land of "+HOME+", I hear the weather is nice around this time of year."
 
+#Stat Prompt's
+print "Tell me more about yourself!"
+STR= raw_input("What is your strength value?")
+DEX= raw_input("What is your dexterity value?")
+LUK= raw_input("How much of a wizard are you?(0-3!)")
