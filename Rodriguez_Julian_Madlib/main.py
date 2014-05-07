b11057e50ed70cf0ac42b414a924a4c8e2d94d83
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-05-06 21:41:08 -0400

    Third Prompt

--- Rodriguez_Julian_Madlib/main.py
+++ Rodriguez_Julian_Madlib/main.py
@@ -5,3 +5,11 @@ print "Well hello hail and harty traveler, Who may I ask your name?"
 NAME= raw_input(" Full Name?:")
 print "Ahhh so your name is "+NAME+ " ....I see."
 
+#Second Prompt
+CLASS= raw_input(" What Class are you?: _")
+print "Ohh so you are a mighty, "+CLASS+", Very interesting..."
+
+#Third Prompt
+HOME= raw_input("Where do you hail from?:")
+print "I see so you come from the land of "+HOME+", I hear the weather is nice around this time of year."
+
