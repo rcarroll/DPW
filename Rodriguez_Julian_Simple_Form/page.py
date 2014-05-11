135f08eda91d543466a852e2ba8ccc20e4b81569
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-05-11 13:28:26 -0400

    Variable set up

---
+++ Rodriguez_Julian_Simple_Form/page.py
@@ -0,0 +1,13 @@
+##refrence page
+
+class HTMLPage(object):
+    def __init__(self):
+
+        self.page_open=''''''
+
+        self.page_content=''''''
+
+        self.page_close= ''''''
+
+        def print_out(self):
+            return self.page_open + self.page_content + self.page_close
